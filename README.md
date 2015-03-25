753f1b67e5dc4fe7a9ecc0ba148f18610f5071b3
kal858 <kl_2k4@yahoo.ca>
2015-03-24 19:00:46 -0600

    added new go source file to repo

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
-# hello
+# Overview
 A test repo for go code
+
+## Purpose
+This file will be for testing go code on my local machine
---
+++ hello.go
@@ -0,0 +1,10 @@
+package main
+
+import (
+	"fmt"
+	"github.com/kal858/string"
+)
+
+func main(){
+	fmt.Println(string.Reverse("Hello, world!\n"))
+}
